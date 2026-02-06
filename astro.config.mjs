92e838ac2126ed9a9c1da5ebf542d1c638cc0a18
Pablo Sangueza <sangueza.p@gmail.com>
2026-02-06 11:33:30 -0400

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://pablosangueza.github.io',
-  base: '//',
+  base: '/iron-stack-web/',
   output: 'static',
   integrations: [tailwind(), sitemap()]
 });
