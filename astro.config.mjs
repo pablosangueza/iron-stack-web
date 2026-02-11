5fe9a7ecddacaddb75cc13debbb04b712761be61
Pablo Sangueza <sangueza.p@gmail.com>
2026-02-10 22:26:32 -0400

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://pablosangueza.github.io',
-  base: '/iron-stack-web/',
+  site: 'https://pablosangueza.github.io/iron-stack-web/',
+  base: '',
   output: 'static',
   integrations: [tailwind(), sitemap()]
 });
